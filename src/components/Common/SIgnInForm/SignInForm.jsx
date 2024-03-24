3cd9c7099c303d2793a3c1ac60efe97c1f08706c
ViktorHim <vhimlihmail@gmail.com>
2024-03-24 21:03:44 +0300

    complete-issue

--- src/components/Common/SIgnInForm/SignInForm.jsx
+++ src/components/Common/SIgnInForm/SignInForm.jsx
@@ -3,6 +3,7 @@ import Button from '../../UI/Button/Button';
 import { Link, useLocation } from 'react-router-dom';
 import { signInUser } from '../../../store/slices/authentication/thunks';
 import { useAuthentication } from '../../../hooks/useAuthentication';
+import Error from '../../UI/Error/Error';
 
 const SignInForm = () => {
 
@@ -20,8 +21,15 @@ const SignInForm = () => {
     <div>
         <h1>Войти</h1>
         <form onSubmit={onSendForm}>
-            <Input name='email' type="email" placeholder='Адрес почты' value={fields.email} onChange={onChange}/>
+            <Input
+            name='email'
+            type="email"
+            placeholder='Адрес почты'
+            value={fields.email}
+            onChange={onChange}
+            />
             <Input name='password' type="password" placeholder='Пароль' value={fields.password} onChange={onChange}/>
+            <Error></Error>
             <Button>Войти</Button>
         </form>
         <div style={{display: 'flex'}}>
